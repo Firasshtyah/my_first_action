308b841b3aa65e3bc1f0322365039ee52453e090
Firasshtyah <96798896+Firasshtyah@users.noreply.github.com>
2022-12-18 15:21:55 +0300

    pull

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ import requests
 request = requests.get('https://w3schools.com')
 
 
-print(f'firas status code is : {request.status_code}')
+print(f'firas pull status code is : {request.status_code}')
